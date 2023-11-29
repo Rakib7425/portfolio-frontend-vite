a9dcd9de47d92ffbd8ac240274021209486026c9
rakibul Islam <irakibul026@gmail.com>
2023-11-29 21:40:24 +0530

    update api's

--- src/hooks/useThemeSwitcher.jsx
+++ src/hooks/useThemeSwitcher.jsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const useThemeSwitcher = () => {
 	const [theme, setTheme] = useState(localStorage.theme);
-	const activeTheme = theme === 'dark' ? 'light' : 'dark';
+	const activeTheme = theme === "dark" ? "light" : "dark";
 
 	useEffect(() => {
 		const root = window.document.documentElement;
 
 		root.classList.remove(activeTheme);
 		root.classList.add(theme);
-		localStorage.setItem('theme', theme);
+		localStorage.setItem("theme", theme);
 	}, [theme, activeTheme]);
 
 	return [activeTheme, setTheme];
