1e0d46ff087641f7d826171e3a57c88c93dbdcff
rakibul Islam <irakibul026@gmail.com>
2023-12-27 23:37:32 +0530

    fix css

--- src/admin/components/adminDashboard/MyProjects.jsx
+++ src/admin/components/adminDashboard/MyProjects.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const MyProjects = () => {
 	return (
 		<motion.div
-			className=' w-full h-80'
+			className=' w-full min-h-64'
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1, delay: 1 }}
 			transition={{
